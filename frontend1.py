c2a6a10014d097c1fb722529f7239bbc95388a99
Thomastouma <42805667+Thomastouma@users.noreply.github.com>
2018-11-07 10:33:52 +0100

    updated frontend1

--- frontend1.py
+++ frontend1.py
@@ -17,7 +17,7 @@ def get_selected_row(event):
 
 # Function call from back2 where row equals all data in sql database "toumatest" TABLE book
 def view_command():
-    list1.delete(0, END)
+    list1.delete(0, END)  # Updates the wiew by deleting the old and printing the new one
     for row in back2.view():
         list1.insert(END, row)
 
@@ -25,7 +25,7 @@ def view_command():
 # Function call from back2
 def add_command():
     back2.insert(title_text.get(), author_text.get(), year_text.get())
-    list1.delete(0, END)
+    list1.delete(0, END)  # Updates the wiew by deleting the old and printing the new one
     view_command()
 
 
@@ -44,34 +44,34 @@ window = Tk()
 window.wm_title("BookStore")
 
 # Labels used in the program, packed in and placed with grid
-l1 = Label(window, text="Title", fg="blue", bg="White")
+l1 = Label(window, text="Title", fg="blue", bg="White")  # Naming it and choosing colors
 l1.grid(row=0, column=0, sticky=N+S+E+W)
 
-l2 = Label(window, text="Author", fg="blue", bg="white")
+l2 = Label(window, text="Author", fg="blue", bg="white")  # Naming it and choosing colors
 l2.grid(row=0, column=2, sticky=N+S+E+W)
 
-l3 = Label(window, text="Year", fg="blue", bg="white")
+l3 = Label(window, text="Year", fg="blue", bg="white")  # Naming it and choosing colors
 l3.grid(row=1, column=0, sticky=N+S+E+W)
 
 # Entry windows used in the program to make it interactive, packed in with grid.
-title_text = StringVar()
+title_text = StringVar()  # Enables the program to retrieve and use the user input in the entry
 e1 = Entry(window, textvariable=title_text)
-e1.grid(row=0, column=1, sticky=N+S+E+W)
+e1.grid(row=0, column=1, sticky=N+S+E+W)  # Sticky stretches the entry window in all directions
 
-author_text = StringVar()
+author_text = StringVar()  # Enables the program to retrieve and use the user input in the entry
 e2 = Entry(window, textvariable=author_text)
-e2.grid(row=0, column=3, sticky=N+S+E+W)
+e2.grid(row=0, column=3, sticky=N+S+E+W)  # Sticky stretches the entry window in all directions
 
-year_text = StringVar()
+year_text = StringVar()  # Enables the program to retrieve and use the user input in the entry
 e3 = Entry(window, textvariable=year_text)
-e3.grid(row=1, column=1, sticky=N+S+E+W)
+e3.grid(row=1, column=1, sticky=N+S+E+W)  # Sticky stretches the entry window in all directions
 
 # Listbox and scrollbar configuration
-list1 = Listbox(window, height=6, width=35)
-list1.grid(row=2, column=0, rowspan=6, columnspan=2, sticky=N+S+E+W)
+list1 = Listbox(window, height=6, width=35)  # Creating the size of the listbox
+list1.grid(row=2, column=0, rowspan=6, columnspan=2, sticky=N+S+E+W)  # Sticky stretches the listbox in all directions
 
 sb1 = Scrollbar(window)
-sb1.grid(row=2, column=2, rowspan=6, sticky=N+S+E+W)
+sb1.grid(row=2, column=2, rowspan=6, sticky=N+S+E+W)  # Sticky stretches the scrollbar in all directions
 
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
